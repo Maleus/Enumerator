3fd2634cac92b1a412a90a6e5ef83810377e8bd3
Steve Coward <steve.coward@gmail.com>
2014-09-08 20:48:12 -0400

    adds distribution-related files.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='enumerator',
+    version='0.1.0',
+    author='Erik Dominguez, Steve Coward',
+    author_email='maleus@placeholder.com, steve@sugarstack.io',
+    packages=['enumerator'],
+    url='http://pypi.python.org/pypi/enumerator/',
+    license='LICENSE.txt',
+    description='enumerator is a tool built to assist in automating the often tedious task of enumerating a target or list of targets during a penetration test.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'blinker==1.3',
+    ],
+)
