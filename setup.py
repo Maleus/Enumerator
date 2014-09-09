0c6f4fdc5db5ac8e94a456ec1ce81be2d51516ca
Steve Coward <steve.coward@gmail.com>
2014-09-08 20:55:45 -0400

    updates gitignore, last change to setup.py - i promise.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Erik Dominguez, Steve Coward',
     author_email='maleus@placeholder.com, steve@sugarstack.io',
-    packages=[''],
+    packages=['', 'lib', 'lib.ftp', 'lib.http', 'lib.nbt'],
     url='http://pypi.python.org/pypi/enumerator/',
     license='LICENSE.txt',
     description='enumerator is a tool built to assist in automating the often tedious task of enumerating a target or list of targets during a penetration test.',
